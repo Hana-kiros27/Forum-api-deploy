85cd0d7599257fa69ee2b1bda0802d8fb6efff58
Hana Hailekiros <hanahailekiros27@gmail.com>
2025-07-01 07:03:06 +0200

    lastly

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -5,12 +5,12 @@ const dotenv = require("dotenv");
 dotenv.config();
 //multiple simeltaneous connection/ create connection pool /reuse/cach
 const dbConnection = mysql2.createPool({
-  port:process.env.PORT,
+  port: process.env.PORT,
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  connectionLimit: 10, // Optional: Set a reasonable mAX limit
+  connectionLimit: process.env.DB_CONNECTION_LIMIT, // Optional: Set a reasonable mAX limit
   waitForConnections: true, // Optional: Wait for available connections
   queueLimit: 0, // Optional: No limit on queue requset size
 });
